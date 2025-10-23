21581f99daf48d0089a6e23fea034d29ffc62917
Egor Ulybin <egorulybin@gmail.com>
2025-10-23 16:45:23 +0200

    start and stop timers button working with all timers

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,23 +1,30 @@
 import Container from './UI/Container.tsx';
 import type { Timer as TimerProps } from '../context/TimersContext.tsx';
 import { useEffect, useState } from 'react';
+import { useTimers } from '../hooks/useTimer.tsx';
 
 export default function Timer({ name, duration }: TimerProps) {
     const durationInMilliseconds = duration * 1000;
     const [remainingTime, setRemainingTime] = useState(durationInMilliseconds);
 
+    const { isRunning } = useTimers();
+
     useEffect(() => {
-        const timer = setInterval(() => {
-            setRemainingTime((prevState) => {
-                if (prevState <= 50) {
-                    clearInterval(timer);
-                    return 0;
-                }
-                return prevState - 50;
-            });
-        }, 50);
+        let timer: number;
+        if (isRunning) {
+            timer = setInterval(() => {
+                setRemainingTime((prevState) => {
+                    if (prevState <= 50) {
+                        clearInterval(timer);
+                        return 0;
+                    }
+                    return prevState - 50;
+                });
+            }, 50);
+        }
+
         return () => clearInterval(timer);
-    }, []);
+    }, [isRunning]);
 
     const displayRemainingTime = (remainingTime / 1000).toFixed(2);
 
